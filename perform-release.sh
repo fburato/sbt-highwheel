60060deaf2c6e879bb9419d3528f8e2ca37d75bc
Francesco Burato <francesco.burato@gmail.com>
2018-04-15 11:27:24 +0100

    Update perform release, update version

--- perform-release.sh
+++ perform-release.sh
@@ -2,7 +2,7 @@
 # Move the .gnupg folder to avoid signature clashing
 # Update version in version.sbt
 # Commit and tag
-sbt publishSigned sonatypeRelease
+sbt clean compile publishSigned sonatypeRelease
 # Update version in version.sbt to add the snapshot
 # Commit
 # Push everything
